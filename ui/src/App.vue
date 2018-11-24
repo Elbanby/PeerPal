79d37a7fd597ccaa8d94bd1557dee1b897733f21
Omar Elbanby <omar.el-banby2@gwl.ca>
2018-11-24 12:55:17 -0500

    Add Header

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{appName}}</h1>
+    <nav-header :naveItems="menuItems"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavHeader from './components/Header.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      appName: 'PeerPal',
+      menuItems: [
+        {title:'About us', link: '#aboutus'},
+        {title: 'How it works', link: '#howitworks'},
+        {title: 'Customer service', link: '#customerservice'}
+      ]
+    }
+  },
+  components: {NavHeader}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import '../styles/globalStyles.css';
 </style>
---
+++ ui/src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="nav_container">
+    <div v-for="naveItem in naveItems" class="nav-item"><a :href="naveItem.link">{{naveItem.title}}</a></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'nav-header',
+    data() {
+      return {
+        name: 'Header'
+      }
+    },
+    props: ['naveItems']
+  }
+</script>

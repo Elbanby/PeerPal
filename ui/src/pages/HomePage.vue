a2d8c92c4f3f1a56a888c5a643c4a2a620c05572
Devon Fazekas <thomas-d@hotmail.ca>
2018-11-25 04:20:58 -0500

    Nav UI done.

--- ui/src/pages/HomePage.vue
+++ ui/src/pages/HomePage.vue
@@ -3,7 +3,25 @@
     <simple-page>
       <div slot="page-content">
         <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-          <a class="navbar-brand" href="#"></a>
+          <a class="navbar-brand" href="#">PeerPal</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Loan</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Deposit</a>
+              </li>
+            </ul>
+          </div>
+
         </nav>
       </div>
     </simple-page>

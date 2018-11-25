da3fcdf9c5d95339a38c491b53a2b86f2d7303d1
Omar Elbanby <omar.el-banby2@gwl.ca>
2018-11-25 04:12:10 -0500

    Fix bug

--- ui/src/pages/HomePage.vue
+++ ui/src/pages/HomePage.vue
@@ -16,14 +16,13 @@ import io from 'socket.io-client';
 
 export default {
   name: 'home-page',
-  data() { 
+  data() {
     return {
       message: '',
       socket : io('http://localhost:3000/')
     }
   },
   components: {SimplePage},
-<<<<<<< HEAD
   mounted() {
     this.socket.emit('SEND_MESSAGE', {
       user: localStorage.getItem('userName'),
@@ -35,24 +34,6 @@ export default {
       console.log('Data ');
       console.log(data);
     });
-=======
-  computed: {
-    send() {
-      this.socket.emit('SEND_MESSAGE', {
-        user: 'omar',
-        message: 'I am trying to connect for fuck sake!'
-      });
-      this.message = '';
-      return null;
-    }
-  },
-  mounted() { 
-    /*this.socket.on('MESSAGE', (data) => {
-      this.messages = [...this.messages, data];
-      console.log('Message' ,this.messages);
-      // you can also do this.messages.push(data)
-    });*/
->>>>>>> 25b3183bdada890a1117780c2bc3330bc4d53491
   }
 }
 </script>

25b3183bdada890a1117780c2bc3330bc4d53491
Devon Fazekas <thomas-d@hotmail.ca>
2018-11-25 04:07:11 -0500

    Fix

--- ui/src/pages/HomePage.vue
+++ ui/src/pages/HomePage.vue
@@ -2,32 +2,9 @@
   <div>
     <simple-page>
       <div slot="page-content">
-        <!-- <nav class="navbar navbar-inverse">
-          <div class="container-fluid">
-            <div class="navbar-header">
-              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
-              
-              <a class="navbar-brand" href="#">PeerPal</a>
-            </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-              <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Tutorials</a></li>
-                <li><a href="#">Customer service</a></li>
-              </ul>
-            </div>
-          </div>
-        </nav> -->
-        <!-- <div class="container">
-          <div class="row">
-            
-          </div>
-        </div> -->
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+          <a class="navbar-brand" href="#"></a>
+        </nav>
       </div>
     </simple-page>
   </div>

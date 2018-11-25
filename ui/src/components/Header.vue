72ddd7c16d267929062647af88f0eae13019ab61
Devon Fazekas <thomas-d@hotmail.ca>
2018-11-25 04:32:09 -0500

    Replaced Home UI and fix mobile nav expansion.

--- ui/src/components/Header.vue
+++ ui/src/components/Header.vue
@@ -1,10 +1,48 @@
 <template>
   <div class="ignore">
-    <ul class="nav justify-content-center ">
+    <!-- <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+      <a class="navbar-brand" href="#">PeerPal</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Loan</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Deposit</a>
+          </li>
+        </ul>
+      </div>
+    </nav> -->
+
+  <b-navbar toggleable="md" type="dark" variant="info">
+
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+  <b-navbar-brand href="#">PeerPal</b-navbar-brand>
+
+  <b-collapse is-nav id="nav_collapse">
+
+    <b-navbar-nav>
+      <b-nav-item active href="#">Home</b-nav-item>
+      <b-nav-item href="#">Loan</b-nav-item>
+      <b-nav-item href="#">Deposit</b-nav-item>
+    </b-navbar-nav>
+
+  </b-collapse>
+</b-navbar>
+
+    <!-- <ul class="nav justify-content-center ">
       <li v-for="naveItem in naveItems" class="nav-link" :key="naveItem.key">
         <a :href="naveItem.link">{{naveItem.title}}</a>
       </li>
-    </ul>
+    </ul> -->
     <!-- <nav class="nav navbar-expand-lg navbar-light bg-light">
       <a class="navbar-brand" href="#">PeerPal</a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">

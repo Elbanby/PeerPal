8623febb190fa33880719c5dbc1f89c8a57cb069
Devon Fazekas <thomas-d@hotmail.ca>
2018-11-25 05:57:43 -0500

    Home UI progress animation.

--- ui/src/pages/HomePage.vue
+++ ui/src/pages/HomePage.vue
@@ -2,17 +2,18 @@
   <div>
     <simple-page>
       <div slot="page-content">
-        <div>
+        <b-col class="mt-4">
           <h1>Balance</h1>
-          <b-progress :value="'$56.43'" :max="100" show-progress striped variant="success"></b-progress>
-          <b-progress class="mt-1" :max="100" show-value>
-            <b-progress-bar :value="45*(6/10)" variant="success"></b-progress-bar>
-            <b-progress-bar :value="45*(2.5/10)" variant="warning"></b-progress-bar>
-            <b-progress-bar :value="45*(1.5/10)" variant="danger"></b-progress-bar>
+          <b-progress class="mt-1" height="4rem" :max="max" show-value>
+            <b-progress-bar :value="98.12" variant="success" :label="'$98.12'"></b-progress-bar>
+            <b-progress-bar :value="loan" variant="danger" :label="'$'+loan.toFixed(2)" animated></b-progress-bar>
           </b-progress>
+          
+          <div class="float-right">Requested: $200.00</div>
 
-          <b-btn class="mt-4" @click="clicked">Click me</b-btn>
-        </div>
+          <b-btn class="mt-5 col-md-12" @click="clicked">Cancel</b-btn>
+        </b-col>
+        
       </div>
     </simple-page>
   </div>
@@ -27,7 +28,9 @@ export default {
   data() {
     return {
       message: '',
-      socket : io('http://localhost:3000/')
+      socket : io('http://localhost:3000/'),
+      max: 200,
+      loan: 76.34
     }
   },
   components: {SimplePage},
@@ -39,9 +42,19 @@ export default {
     this.message = '';
 
     this.socket.on('MESSAGE',(data)=>{
-      console.log('Data ');
+      console.log('Data');
       console.log(data);
     });
+
+    let interval = setInterval(() => {
+      
+      this.loan = this.loan + 0.01;
+      //console.log(this.loan);
+    }, 900);
+
+    setTimeout(() => {
+      clearInterval(interval);
+    }, 20000);
   }
 }
 </script>
